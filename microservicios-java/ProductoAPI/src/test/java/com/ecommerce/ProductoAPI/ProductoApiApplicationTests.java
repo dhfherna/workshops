ab5ca8a4a5800e1015d08950f1cce22ccaebe0f8
dhfherna <dhfherna@bancolombia.com.co>
2024-12-11 11:24:05 -0500

    Ejercicio microservicios en Spring Boot

---
+++ microservicios-java/ProductoAPI/src/test/java/com/ecommerce/ProductoAPI/ProductoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ProductoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
